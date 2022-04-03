cd3d25cf3745b36e901c6ad7fa18abdc6be2e5b6
unknown <andrew@omnicode.com>
2022-04-03 01:51:21 -0400

    code clean up

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
-    private BoxCollider2D boxCollider2D;
 
-    private float directionX = 0f;
     private float directionY = 0f;
-    [SerializeField] private float moveSpeed = 5f;
-    private float jumpForce = 14f;
-
-    private bool isJumping = false;
 
     [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private float moveSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -30,15 +24,6 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Jump"))
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, moveSpeed);
-            isJumping = true;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            isJumping = false;
         }
     }
 }
