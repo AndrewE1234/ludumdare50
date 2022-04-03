ac6d732a143fabfe70ba765d8b417966a1a3dfb9
unknown <andrew@omnicode.com>
2022-04-03 01:47:49 -0400

    random cloud spawning added

--- Game/Assets/Scripts/CloudSpawner.cs
+++ Game/Assets/Scripts/CloudSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CloudSpawner : MonoBehaviour
 {
     private Vector2 screen;
+    private SpriteRenderer cloudSpriteRenderer;
 
     [SerializeField] private GameObject cloudObject;
     [SerializeField] private float respawnTime = 1f;
@@ -19,6 +20,9 @@ public class CloudSpawner : MonoBehaviour
 
     void spawnCloud()
     {
+        cloudSpriteRenderer = cloudObject.GetComponent<SpriteRenderer>();
+        cloudSpriteRenderer.sprite = sprites[Random.Range(0, sprites.Length - 1)];
+
         GameObject obj = Instantiate(cloudObject);
         obj.transform.position = new Vector2(screen.x * 1.1f, Random.Range(screen.y - 6f, screen.y - 0.5f));
     }
